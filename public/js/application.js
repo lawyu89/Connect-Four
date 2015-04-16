8d2b353694834f42cb0de983f080f2e6f5dc11f8
Lawrence Yu & Lawrence Yu <pair+lawyu89.lawyu89@devbootcamp.com>
2015-04-16 16:53:50 -0700

    Add connect four functionality

--- public/js/application.js
+++ public/js/application.js
@@ -5,22 +5,30 @@ $(document).ready(function() {
   // find empty cell
   var nextEmptyRow = function(columnNumber){
     // rgb(255, 255, 255) is white
-    for (var i = 0; i < $('tr').length; i++) {
-      $('.column1')[i].children.css('background-color')
+    column = $('.'+columnNumber)
+    for (var i = 0; i < column.length; i++) {
+      current = column[i].children
+      if ($(current).css('background-color') !== "rgb(255, 255, 255)") {
+        return $(column[i-1].children)
+      } else if(i===column.length -1) {
+        return $(current)
+      }
     }
 
 
   }
 
-  $(".column1").on("click", function(){
+  $("td").on("click", function(){
     if (counter % 2 === 0){
       piece_color = "black"
     } else {
       piece_color = "red"
     };
-    debugger
+    column_num = $(this).attr('class')
     //$(".row8 .column1 .cell").css("background", piece_color);
     // .attr('class') on this to get class and pass to row_finder
+    available_slot = nextEmptyRow(column_num)
+    $(available_slot).css('background-color',piece_color)
     counter++;
   });
 
