05f4e131f920824703afb94442c5d86ace10c16f
David P. Hanesana & David P. Hanesana <pair+dhanesana.dhanesana@devbootcamp.com>
2015-04-17 12:25:55 -0700

    Add winning condition

--- public/js/application.js
+++ public/js/application.js
@@ -28,29 +28,71 @@ $(document).ready(function() {
     available_slot = nextEmptyRow(column_num)
     $(available_slot).css('background-color',piece_color)
     counter++;
-  });
 
-
-
-
-});
 
 // stringify
-var stringifyBoard = ""
 
-var board = $('.cell')
-for (var i = 0; i < board.length; i++) {
-  var cell = $(board[i])
-  if ( cell.css('background-color') === "rgb(255, 255, 255)"){
-    stringifyBoard + "0"  //white
-  } else if ( cell.css('background-color') === "rgb(0, 0, 0)"){
-    stringifyBoard + "1" //black
-  } else if ( cell.css('background-color') === "rgb(255, 0, 0)"){
-    stringifyBoard + "2" //red
-  }
+    stringifyBoard = ""
+    var board = $('.cell')
+    for (var i = 0; i < board.length; i++) {
+      var cell = $(board[i])
+      if ( cell.css('background-color') === "rgb(255, 255, 255)"){
+        stringifyBoard += "0"  //white
+      } else if ( cell.css('background-color') === "rgb(0, 0, 0)"){
+        stringifyBoard += "1" //black
+      } else if ( cell.css('background-color') === "rgb(255, 0, 0)"){
+        stringifyBoard += "2" //red
+      }
+
+    }
+
+    checkColumn = [[],[],[],[],[],[],[]]
+    checkRow = [[],[],[],[],[],[]]
+    checkLDiagonal = [[],[],[],[],[],[]]
+    checkRDiagonal = [[],[],[],[],[],[]]
+
+    var i = 0;
+    while (i < stringifyBoard.length){
+      checkColumn[i%7].push(stringifyBoard[i]);
+      checkRow[Math.floor(i/7)].push(stringifyBoard[i]);
+      if (i !== 4 && i !== 5 && i !== 6 && i !== 12 && i !== 13 && i !== 20 && i !== 21 && i !== 28 && i !== 29 && i !== 35 && i !== 36 && i !== 37) {
+        if (i % 8 < 4) {
+          checkLDiagonal[i % 8].push(stringifyBoard[i]);
+        } else {
+          checkLDiagonal[(i % 8) - 2].push(stringifyBoard[i]);
+        }
+      }
+      if (i !== 0 && i !== 1 && i !== 2 && i !== 7 && i !== 8 && i !== 14 && i !== 27 && i !== 33 && i !== 34 && i !== 39 && i !== 40 && i !== 41) {
+        checkRDiagonal[i % 6].push(stringifyBoard[i]);
+      }
+      i++;
+    }
+
+    for (var i = 0; i < checkColumn.length; i++) {
+      if (checkColumn[i].join('').search('1111') >= 0 || checkColumn[i].join('').search('2222') >= 0) {
+        alert('u r winner');
+      }
+    }
+    for (var i = 0; i < checkRow.length; i++) {
+      if (checkRow[i].join('').search('1111') >= 0 || checkRow[i].join('').search('2222') >= 0) {
+        alert('u r winner');
+      }
+    }
+    for (var i = 0; i < checkLDiagonal.length; i++) {
+      if (checkLDiagonal[i].join('').search('1111') >= 0 || checkLDiagonal[i].join('').search('2222') >= 0) {
+        alert('u r winner');
+      }
+    }
+    for (var i = 0; i < checkRDiagonal.length; i++) {
+      if (checkRDiagonal[i].join('').search('1111') >= 0 || checkRDiagonal[i].join('').search('2222') >= 0) {
+        alert('u r winner');
+      }
+    }
+
+  });
+});
 
 
 
-}
 
 
